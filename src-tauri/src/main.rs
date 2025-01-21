e5e42aa283eba6eccfcb7a595eb752bfae2ee0fa
Dragorad <mi.e.drago@gmail.com>
2025-01-21 18:59:40 +0200

    first components added

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::command;
+
+#[command]
+fn add_directory(path: String, days: Vec<String>, hours: String, frequency: u32) {
+    println!("Adding directory: {}", path);
+    println!("Days: {:?}", days);
+    println!("Hours: {}", hours);
+    println!("Frequency: {} seconds", frequency);
+    // Тук можете да добавите логика за съхранение на данните или стартиране на наблюдението
+}
+
 fn main() {
-    rust_watcher_lib::run()
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![add_directory])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/StartScreen.jsx
+++ src/StartScreen.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import DirectoriesTable from "./DirectoriesTable"
+import AddEditModal from "./AddEditModal"
 
 export const StartScreen = () => {
-    const [modalOpen, setModalOpen] = useState(false)
+    const [modalOpen, setModalOpen] = useState(true)
     const [notifyScreenOpen, setNotifyScreenOpen] = useState(true)
     return (
         <div>
@@ -12,6 +13,7 @@ export const StartScreen = () => {
             {notifyScreenOpen &&
                 <button>Show Notifications</button>}
             <button>Add directory</button>
+            {modalOpen && <AddEditModal />}
             <div><DirectoriesTable /> </div>
         </div>
 
